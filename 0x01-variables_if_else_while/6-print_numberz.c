1caa4df41cd23113ac0bf64cf8738086383b631e
oluwagbengaakinwande <oluwagbengaakinwande@gmail.comgit config --global user.name oluwagbengaakinwande>
2023-02-18 13:10:27 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar(48);
+	putchar(49);
+	putchar(50);
+	putchar(51);
+	putchar(52);
+	putchar(53);
+	putchar(54);
+	putchar(55);
+	putchar(56);
+	putchar(57);
+	putchar('\n');
+
+	return (0);
+}
+
