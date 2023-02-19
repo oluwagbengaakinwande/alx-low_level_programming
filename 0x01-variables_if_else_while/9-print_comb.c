c1bc5df303281bf3ee59df594570eae41348ae72
oluwagbengaakinwande <oluwagbengaakinwande@gmail.comgit config --global user.name oluwagbengaakinwande>
2023-02-19 03:51:53 -0800

    #include <stdio.h>

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - prints all possible combinations of single-digit numbers using putchar
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Description: prints numbers
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i <= '9' ; i++)
 	{
-		for (j = i; j < 10; j++)
-		{
-			putchar(i + '0');
-			putchar(j + '0');
+		putchar(i);
 
-			if (i != 9 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
 	putchar('\n');
-
 	return (0);
 }
 
