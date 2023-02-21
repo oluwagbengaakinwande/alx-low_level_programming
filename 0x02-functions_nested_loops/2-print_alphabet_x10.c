148133f5c07a5c6ffa748e1f4fc5b817d888068b
oluwagbengaakinwande <oluwagbengaakinwande@gmail.comgit config --global user.name oluwagbengaakinwande>
2023-02-21 06:52:48 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - prints the alphabet in lowercase 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j, c;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 'a';
+	while (j <= 'z')
+	{
+		_putchar (j);
+		j++
+	}
+	_putchar('\n');
+	i++;
+	}
+}
