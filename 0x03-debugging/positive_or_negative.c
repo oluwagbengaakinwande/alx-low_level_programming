d7872576b6d6fc25eb8f8980bb3a280ca8afe28a
oluwagbengaakinwande <oluwagbengaakinwande@gmail.comgit config --global user.name oluwagbengaakinwande>
2023-02-23 09:13:27 -0800

    Task0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * postive_or_negative - check for positive or negative numbers
+ *
+ * @i: the number to be checked
+ *
+ * Return: always 0
+ */
+
+void positive_or_negative(int i);
+{	
+	if (1 < 0)
+		printf("%d is negative\n", i)
+	else
+		printf("%d is positive\n, i")
+}
