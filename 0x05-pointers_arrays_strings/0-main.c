35989dafa39b141481657da78031b3a3809a9350
oluwagbengaakinwande <oluwagbengaakinwande@gmail.comgit config --global user.name oluwagbengaakinwande>
2023-02-27 09:25:01 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -4,15 +4,21 @@
 /**
  * main - check the code 
  *
- * Return: Always 0.
+ * Return: Always 0 
  */
+
+void reset_to_98(int *n);
+
 int main(void)
 {
-	 int n;
-
-	n = 402;
-	printf("n=%d\n", n);
+	int n = 402;
+	printf("n = %d\n", n);
 	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+	printf("n = %d\n", n);
+	return 0;
+	}
+
+	void reset_to_98(int *n)
+	{
+	*n = 98;
 }
