e19a566ed1fffd4a57343374c6f60e2576fb9ada
oluwagbengaakinwande <oluwagbengaakinwande@gmail.comgit config --global user.name oluwagbengaakinwande>
2023-02-27 18:39:08 -0800

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _puts - Prints a string
+ * @str: The string to print
+ * Return: void
+ * */
+
+void _puts(char *str) 
+{
+	int i = 0;
+    
+	while (str[i] != '\0') 
+	{
+	putchar(str[i]);
+	i++;
+	}
+	putchar('\n');
+}
