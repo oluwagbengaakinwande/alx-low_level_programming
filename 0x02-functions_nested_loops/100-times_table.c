3ab7487bcd6fee3fdc8ba30fdb10da2433c4d5ec
oluwagbengaakinwande <oluwagbengaakinwande@gmail.comgit config --global user.name oluwagbengaakinwande>
2023-03-06 04:30:02 -0800

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,38 +2,46 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: the highest value of the table
- *
- * Return: void
+ * @n: number of the times table
  */
 
 void print_times_table(int n)
-
 {
-	int i, j, result;
+	int i, j, k;
 
-	if (n < 0 || n > 15)
-		return;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			result = i * j;
-
-			if (j == 0)
-				printf("%d", result);
-			else
-				printf(", %d", result / 100);
-
-			if (result >= 100)
-				printf("%d%d", (result / 10) % 10, result % 10);
-			else if (result >= 10)
-				printf(" %d%d", result / 10, result % 10);
-			else
-				printf("  %d", result);
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
 		}
-
-		putchar('\n');
-	}
-}
+}}
