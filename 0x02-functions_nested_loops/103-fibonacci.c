cb77699bc9d23f306c6e41b87607e48151e470b6
oluwagbengaakinwande <oluwagbengaakinwande@gmail.comgit config --global user.name oluwagbengaakinwande>
2023-03-05 19:59:31 -0800

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,8 +11,8 @@ int main(void)
 {
 	int prev = 1, curr = 2, next;
 	int sum = 2;
-	/* initialize with 2 because curr is even */	
-	while (curr <= 4000000)
+/* initialize with 2 because curr is even */	
+while (curr <= 4000000)
 	{
 	next = prev + curr;
 	if (next % 2 == 0)
