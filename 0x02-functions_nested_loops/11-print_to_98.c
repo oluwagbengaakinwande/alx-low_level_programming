d03c5e733af6a11ffbf8b81872fab93cd4e83912
oluwagbengaakinwande <oluwagbengaakinwande@gmail.comgit config --global user.name oluwagbengaakinwande>
2023-03-06 04:42:19 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,27 +1,17 @@
-#include <stdio.h>
-#include <math.h>
+#include "main.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
+
 {
-	long int num = 612852475143;
-	long int i, max = -1;
-
-	while (num % 2 == 0) {
-		max = 2;
-		num /= 2;
-	}
-
-	for (i = 3; i <= sqrt(num); i += 2) {
-	while (num % i == 0) {
-		max = i;
-		num /= i;
-	}
-	}
-
-	if (num > 2)
-	max = num;
-
-	printf("%ld\n", max);
-
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
 	return (0);
 }
