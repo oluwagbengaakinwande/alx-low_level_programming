ac132df54fcc166381a179dc4fe8ef52abe550d7
oluwagbengaakinwande <oluwagbengaakinwande@gmail.comgit config --global user.name oluwagbengaakinwande>
2023-03-07 09:40:35 -0800

    10. a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,29 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * add_integers - adds two integers and returns the result
- * @x: the first integer to add
- * @y: the second integer to add
+ * add - adding it up
+ * @a: first integer
+ * @b: second integer
  *
- * Return: the sum of x and y
+ * Return: sum of both integers
  */
-int add_integers(int x, int y)
+int add(int a, int b)
 {
-	return x + y;
-}
-
-/**
- * main - entry point of the program
- *
- * Return: 0 on success
- */
-int main(void)
-{
-	int a = 3;
-	int b = 4;
-	int result = add_integers(a, b);
-
-	printf("%d + %d = %d\n", a, b, result);
-	return 0;
+	return (a + b);
 }
