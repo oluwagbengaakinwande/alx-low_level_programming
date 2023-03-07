d5cd9fb729a61c68b2a625bd88d07b1ffa22066e
oluwagbengaakinwande <oluwagbengaakinwande@gmail.comgit config --global user.name oluwagbengaakinwande>
2023-03-07 03:08:14 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
-        /**
-         * main - print _putchar
-         * is written by mbah
-         * Return: 0
-         */
+
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
 int main(void)
-	{
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
-	}
+}
